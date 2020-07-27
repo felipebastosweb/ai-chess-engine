70991b06e11b224fc488e330ae088f82a91f7117
felipe bastos <felipebastosweb@gmail.com>
2020-07-27 13:59:10 -0300

    Update board.py

--- game/board.py
+++ game/board.py
@@ -3,4 +3,16 @@ import numpy as np
 class Board:
     def __init__(self):
         self.board = np.zeros(64).reshape(8, 8)
-        
+    # TODO: Verify if is game over
+    def is_game_over(self):
+        pass
+    # TODO: Verify if is c
+    def is_check(self):
+        pass
+    # TODO: Verify if is checkmate
+    def checkmate(self):
+        pass
+    # TODO: convert 
+    def __str__(self):
+        pass
+
